7e8e41e0e6315fc3fefd2b1be53d224e1801722f
weehaa <vzg1000@mail.ru>
2020-11-21 20:29:12 +0400

    add security features to app

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 // HTTP request logger middleware for node.js
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('colors');
 
 // Load env vars
@@ -22,7 +29,30 @@ connectDB();
 const app = express();
 
 // Apply logging middleware in development mode
-process.env.NODE_ENV === 'DEVELOPMENT' && app.use(morgan('dev'));
+process.env.NODE_ENV === 'development' && app.use(morgan('dev'));
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attack
+app.use(xssClean());
+
+// Apply rate limiter
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 1000
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Mount routers
 app.use('/api/v1/languages', languages);
